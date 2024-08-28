ca2dbefc59f7eab2cbb2437dcf672ecc734e9d45
salmankazi <salmanbitw434@gmail.com>
2024-08-28 15:20:00 +0530

    db model queries

---
+++ src/model/projects.model.js
@@ -0,0 +1,29 @@
+// models/Project .js
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Project = sequelize.define('Project', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: Sequelize.NOW,
+  },
+});
+
+module.exports = Project;
